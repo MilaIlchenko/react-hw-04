a6cd684249144ecd4abb99f27c91d504289f767b
MilaIlchenko <147260557+MilaIlchenko@users.noreply.github.com>
2024-05-20 10:17:02 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { fetchImages } from "./image-api";
 
-function App() {
-  const [count, setCount] = useState(0)
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
+import NotFoundError from "./components/NotFoundError/NotFoundError";
+
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [notFoundError, setNotFoundError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const [likes, setLikes] = useState(0);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    if (!query) return;
+
+    const getImages = async () => {
+      try {
+        setError(false);
+        setLoading(true);
+        setNotFoundError(false);
+
+        const newImages = await fetchImages(page, query);
+
+        if (newImages.length === 0) {
+          setNotFoundError(true);
+        } else {
+          setImages((prevImages) => [...prevImages, ...newImages]);
+        }
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getImages();
+  }, [query, page]);
+
+  const handleSubmit = (query) => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
+  };
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const openModal = (url, like, nameUser) => {
+    setImageUrl(url);
+    setLikes(like);
+    setUserName(nameUser);
+    toggleModal();
+  };
+
+  const toggleModal = () => {
+    setModal((prevModal) => !prevModal);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+      {images.length > 0 && <ImageGallery onImgClick={openModal} items={images} />}
+      {notFoundError && <NotFoundError />}
+      {error && <ErrorMessage />}
+      {loading && <Loader />}
+      {images.length > 0 && !loading && <LoadMoreBtn onClick={handleLoadMore} />}
+      {modal && (
+        <ImageModal
+          image={imageUrl}
+          imgModal={modal}
+          item={images}
+          onModalClose={toggleModal}
+          imgLikes={likes}
+          user={userName}
+        />
+      )}
+    </div>
+  );
 }
-
-export default App
